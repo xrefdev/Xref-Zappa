028e27e44aacfd837db7fba52ad96f98711316f8
Brett <brett_matthews@live.com.au>
2019-12-03 14:21:59 +1100

    update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-nosetests --with-coverage --cover-package=zappa
+nosetests --with-coverage --cover-package=xrefzappa
 
 # For a specific test:
 # nosetests tests.tests:TestZappa.test_lets_encrypt_sanity -s
